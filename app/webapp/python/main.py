bd442aca8d6ac89c9fcadee3c66cff8210a7597a
Connor <mclarenc@hotmail.co.uk>
2025-04-11 16:15:31 +0900

    Updated main/index

--- app/webapp/python/main.py
+++ app/webapp/python/main.py
@@ -2,16 +2,26 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
+mock_decks = {
+    "japanese_basic": [
+        {"id": 1, "front": "犬", "back": "Dog"},
+        {"id": 2, "front": "猫", "back": "Cat"},
+    ],
+}
 
 # To implement ---------------------------------------------------
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('templates/index.html')
 
 
 @app.route('/api/getDeckCards/<deckname>', methods=['GET'])
 def get_deck_cards(deckname):
-    pass
+    # Fetch deck by name, or return an empty list if not found
+    deck = mock_decks.get(deckname)
+
+    # Return the flashcards as JSON
+    return jsonify(deck)
 
 
 @app.route('/api/getFlashcard/<cardID>', methods=['GET'])
